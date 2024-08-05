0835670e839d31be7518851a77ee42989c3f1495
danielram12393 <danram1001@gmail.com>
2024-08-05 03:25:48 -0500

    Added text fields for input and output. Added conversion button. Added error message, and working on error handling

--- src/UnitConverter.java
+++ src/UnitConverter.java
@@ -6,13 +6,16 @@ import java.awt.event.ActionListener;
 public class UnitConverter extends JFrame implements ActionListener {
     JPanel panel;
     JButton convert;
+    JTextField leftField, rightField;
+    JLabel errorMessage;
 
+    GridBagConstraints c;
 
-    public static final Font TMRFont = new Font("Times New Roman", 0, 40);
+     static final Font TIMES_NEW_ROMAN = new Font("Times New Roman", Font.PLAIN, 40);
+     static final Font ERROR_FONT = new Font("Times New Roman", Font.BOLD, 40);
 
     public UnitConverter() {
         super("Unit Converter");
-
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
@@ -20,7 +23,30 @@ public class UnitConverter extends JFrame implements ActionListener {
 
         conversionButtonConfig();
 
-        panel.add(convert);
+        textFieldConfig();
+
+        errorMessageConfig();
+
+        //Positioning of all components
+        {
+            c.gridx = 0;
+            c.gridy = 1;
+            panel.add(leftField, c);
+
+            c.gridx = 1;
+            c.gridy = 1;
+            panel.add(rightField, c);
+
+            c.gridx = 0;
+            c.gridy = 2;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+            panel.add(convert, c);
+
+            c.gridx = 0;
+            c.gridy = 3;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+            panel.add(errorMessage, c);
+        }
 
         add(panel);
         setVisible(true);
@@ -30,17 +56,49 @@ public class UnitConverter extends JFrame implements ActionListener {
     private void mainPanelConfig() {
         panel = new JPanel();
         panel.setPreferredSize(new Dimension(700,500));
+        panel.setLayout(new GridBagLayout());
+        c = new GridBagConstraints();
     }
 
     private void conversionButtonConfig() {
         convert = new JButton("Convert");
-        convert.setFont(TMRFont);
+        convert.setPreferredSize(new Dimension(300, 80));
+        convert.setFont(TIMES_NEW_ROMAN);
         convert.addActionListener(this);
         convert.setFocusable(false);
     }
 
+    private void textFieldConfig() {
+        leftField = new JTextField();
+        rightField = new JTextField();
+
+        leftField.setFont(TIMES_NEW_ROMAN);
+        rightField.setFont(TIMES_NEW_ROMAN);
+
+        leftField.setPreferredSize(new Dimension(200, 100));
+        rightField.setPreferredSize(new Dimension(200, 100));
+
+        rightField.setEditable(false);
+    }
+
+    private void errorMessageConfig() {
+        errorMessage = new JLabel();
+        errorMessage.setPreferredSize(new Dimension(500,40));
+        errorMessage.setFont(ERROR_FONT);
+
+        errorMessage.setForeground(Color.red);
+        errorMessage.setBackground(Color.black);
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        System.out.println(1);
+        String input = leftField.getText();
+
+        if(!input.matches("^\\d*\\.\\d+|\\d+\\.\\d*$")) {
+            errorMessage.setText("Enter a valid number!");
+        } else {
+            rightField.setText(input);
+        }
     }
 }
